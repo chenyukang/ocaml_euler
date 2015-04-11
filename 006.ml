1c7bfe395037d88e9f3381b74d96af6b938e6981
Yukang <moorekang@gmail.com>
2015-04-11 09:56:52 +0800

    sqaure sum

---
+++ 006.ml
@@ -0,0 +1,21 @@
+
+
+let solve n =
+  let s1 = ref 0 in
+  let s2 = ref 0 in 
+  for x = 1 to n do
+    s1 := !s1 + (x * x);
+  done;
+
+  for x = 1 to n do
+    s2 := !s2 + x;
+  done;
+  s2 := !s2 * !s2;
+  !s2 - !s1
+         
+
+let () =
+  assert(solve 10 = 2640);
+  assert(solve 100 = 25164150);
+  
+    
